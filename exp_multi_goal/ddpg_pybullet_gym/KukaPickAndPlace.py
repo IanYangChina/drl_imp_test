e3af700b912222a61f5fb95252aa871dd743ed16
Xintong <xintongyang1994@foxmail.com>
2021-03-10 11:52:40 +0000

    new exps

--- exp_multi_goal/ddpg_pybullet_gym/KukaPickAndPlace.py
+++ exp_multi_goal/ddpg_pybullet_gym/KukaPickAndPlace.py
@@ -38,7 +38,14 @@ path = os.path.join(path, 'PickAndPlace')
 
 for seed in seeds:
 
-    env = pmg.make("KukaParallelGripPickAndPlaceSparseEnv-v0")
+    env = pmg.make_env(task='pick_and_place',
+                       gripper='parallel_jaw',
+                       render=False,
+                       binary_reward=True,
+                       max_episode_steps=50,
+                       image_observation=False,
+                       depth_image=False,
+                       goal_image=False)
 
     seed_path = path + '/seed'+str(seed)
 
