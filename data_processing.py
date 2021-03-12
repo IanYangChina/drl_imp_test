25f7ecfa42f1109baaaa9bec1cfa6d845e0399fb
xintong <xintongyang@foxmail.com>
2021-03-12 23:19:35 +0000

    codes

--- data_processing.py
+++ data_processing.py
@@ -4,23 +4,25 @@ import json
 
 path = os.getcwd()
 
-mujoco_ddpg = json.load(open(os.path.join(path, 'exp_multi_goal', 'ddpg_mujoco_gym', 'Reach', 'success_rate_statistic.json')))
-mujoco_ddpg_her = json.load(open(os.path.join(path, 'exp_multi_goal', 'ddpg_mujoco_gym', 'Reach_HER', 'success_rate_statistic.json')))
-bullet_ddpg = json.load(open(os.path.join(path, 'exp_multi_goal', 'ddpg_pybullet_gym', 'Reach', 'success_rate_statistic.json')))
-bullet_ddpg_her = json.load(open(os.path.join(path, 'exp_multi_goal', 'ddpg_pybullet_gym', 'Reach_HER', 'success_rate_statistic.json')))
+env = 'PickAndPlace'
 
-plot.smoothed_plot_mean_deviation(file=os.path.join(path, 'src', 'success_rate_reach'),
+mujoco_ddpg = json.load(open(os.path.join(path, 'exp_multi_goal', 'ddpg_mujoco_gym', env, 'success_rate_statistic.json')))
+mujoco_ddpg_her = json.load(open(os.path.join(path, 'exp_multi_goal', 'ddpg_mujoco_gym', env+'_HER', 'success_rate_statistic.json')))
+bullet_ddpg = json.load(open(os.path.join(path, 'exp_multi_goal', 'ddpg_pybullet_gym', env, 'success_rate_statistic.json')))
+bullet_ddpg_her = json.load(open(os.path.join(path, 'exp_multi_goal', 'ddpg_pybullet_gym', env+'_HER', 'success_rate_statistic.json')))
+
+plot.smoothed_plot_mean_deviation(file=os.path.join(path, 'src', 'success_rate_'+env),
                                   data_dict_list=[mujoco_ddpg, mujoco_ddpg_her, bullet_ddpg, bullet_ddpg_her],
                                   file_formats=[
                                       'pdf',
                                       'png'
                                   ],
-                                  ylim=(0.68, 1.02),
+                                  ylim=(-0.05, 1.05),
                                   legend=['Mujoco-DDPG',
                                           'Mujoco-DDPG-HER',
                                           'Bullet-DDPG',
                                           'Bullet-DDPG-HER'],
-                                  legend_title='Reach (4 seeds)',
+                                  legend_title=env+' (4 seeds)',
                                   legend_loc='lower left', legend_bbox_to_anchor=(0.1, 0.98), legend_ncol=2, legend_frame=False,
                                   window=1,
                                   x_label='Epoch', y_label='Average test returns')
